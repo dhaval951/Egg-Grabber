e23aa7c6f9d855d9d683efa6dcbdd81d54871815
Dhaval Bhadania <dhaval.bhadania@ZS-10.local>
2014-02-03 11:00:44 +0530

    Initial Commit

---
+++ eggate/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  eggate
+//
+//  Created by Dhaval Bhadania on 03/02/14.
+//  Copyright Dhaval Bhadania 2014. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
